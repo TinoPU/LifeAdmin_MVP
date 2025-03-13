67111f240547dc4645090f7587c54bf61fff0a56
Tino Purmann <tinopurmann@gmail.com>
2025-03-14 00:30:57 +0100

    reformatted conversation history

--- src/utils/redisActions.ts
+++ src/utils/redisActions.ts
@@ -5,7 +5,7 @@ import {User} from "../types/db";
 export async function cacheWhatsappMessage(user: User, actor: string, message: string, timestamp: string) {
 
     const redisKey = `conversation:${user.id}`;
-    await redisClient.lPush(redisKey, JSON.stringify({ role: actor, message: message, timestamp: timestamp }));
+    await redisClient.lPush(redisKey, JSON.stringify({ timestamp: timestamp, role: actor, message: message },null, 2));
     await redisClient.lTrim(redisKey, 0, 9); // Keep last 10 messages
 
 }
