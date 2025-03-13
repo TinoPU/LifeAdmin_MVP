f888840b3b7d497fadf95e4c71072d8ebe4d387a
Tino Purmann <tinopurmann@gmail.com>
2025-03-14 00:55:52 +0100

    fixed redis formatting

--- src/services/conversationService.ts
+++ src/services/conversationService.ts
@@ -4,7 +4,7 @@ class ConversationService {
     async getRecentMessages(user_id: string) {
         const redisKey = `conversation:${user_id}`;
         const recentMessages = await redisClient.lRange(redisKey, 0, 9);
-        return recentMessages.reverse().map(msg => JSON.parse(msg)); // Reverse for chronological order
+        return recentMessages.reverse() // Reverse for chronological order
     }
 }
 
